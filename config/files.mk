a5611ecf5823b3bb54b17c854ea9e59a4d7f7b77
sam0verfl0w <samtalespro@gmail.com>
2024-01-09 09:39:08 +0100

    update makefile

---
+++ config/files.mk
@@ -0,0 +1,77 @@
+include ./config/project.mk
+
+# Compiler
+
+# GCC Default
+
+ifeq ($(EXT_FILE_PROJECT), c)
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -I$(INCS_DIR) -pedantic -DVERSION=\"$(VERSION)"
+LDFLAGS =
+COMPILER_VERSION = $(shell $(CC) --version | head -n1)
+endif
+
+# Assembly Compiler using GAS as Default
+
+ifeq ($(EXT_FILE_PROJECT), s)
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -I$(INCS_DIR) -pedantic -DVERSION=\"$(VERSION)"
+LDFLAGS =
+COMPILER_VERSION = $(shell $(CC) --version | head -n1)
+endif
+
+# Flags
+
+ifeq ($(IS_LIBRARY), true)
+	CFLAGS += -fPIE
+endif
+
+ifdef CLANG
+	CC = clang
+endif
+
+ifdef FAST
+	CFLAGS += -Ofast
+endif
+
+ifdef O2
+	CFLAGS += -O2
+endif
+
+ifdef O3
+	CFLAGS += -O3
+endif
+
+ifdef OSIZE
+	CFLAGS += -Osize
+endif
+
+ifdef DEBUG
+	CFLAGS += -DDEBUG=1
+endif
+
+ifdef STRIP
+	CFLAGS += -s
+endif
+
+# Source Files
+
+SRCS = $(shell ls $(SRCS_DIR)/*.$(EXT_FILE_PROJECT))
+OBJS = $(addprefix $(OBJDIR)/, $(SRCS:.$(EXT_FILE_PROJECT)=.o))
+
+# Test Files
+
+TEST_SRCS = $(shell ls $(TEST_SRCS_DIR)/*.$(EXT_FILE_PROJECT) 2> /dev/null)
+TEST_OBJS = $(TEST_SRCS:*.$(EXT_FILE_PROJECT)=.test)
+
+# VPath
+
+ifeq ($(EXT_FILE_PROJECT), c)
+vpath %.c $(SRCS_DIR)
+vpath %.h $(INCS_DIR)
+endif
+
+ifeq ($(EXT_FILE_PROJECT), s)
+vpath %.s $(SRCS_DIR)
+vpath %.h $(INCS_DIR)
+endif
---
+++ config/project.mk
@@ -0,0 +1,25 @@
+# Project Info
+#
+PROJECT_NAME = supermath
+IS_LIBRARY = true
+AUTHOR = Unam3dd
+GITHUB = https://github.com/Unam3dd
+VERSION = 0.0.1
+
+# System information
+
+DISTRO = $(shell cat /etc/os-release | grep "^ID" | head -n1 | cut -d '=' -f2)
+WHOAMI = $(shell whoami)
+INTERFACE = $(shell ls /sys/class/net | grep "wl")
+LOCAL_IP = $(shell ifconfig $(shell ls /sys/class/net/ | grep "wl")  | grep "inet" | head -n1 | sed "s/^[[:space:]]*//" | cut -d ' ' -f2)
+
+# Project files configuration
+
+INCS_DIR = inc
+SRCS_DIR = src
+EXT_FILE_PROJECT = $(shell ls src | head -n1 | sed 's/^.*\.//')
+
+# Test Project files configuration
+
+TEST_INCS_DIR = test
+TEST_SRCS_DIR = test
