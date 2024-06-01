56dbf0072ec7fa8cc7b1f105609fb7cd0e992882
Unam3dd <samtalespro@gmail.com>
2024-06-01 21:25:34 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu:22.04
 HEALTHCHECK NONE
 
 RUN apt-get update && \
-    apt-get install git gcc gcc-multilib make python3-pip cmake pkg-config ninja-build --no-install-recommends sudo -y && \
+    apt-get install git gcc gcc-multilib make python3-pip cmake pkg-config ninja-build sudo --no-install-recommends -y && \
     apt-get clean && pip install meson
 
 RUN useradd --create-home nonroot
@@ -12,7 +12,7 @@ WORKDIR /home/nonroot/app
 
 COPY --chown=nonroot:nonroot . /home/nonroot/app
 
-RUN make release
+RUN make release clean
 
 USER nonroot
 
--- tests/mylib.c
+++ tests/mylib.c
@@ -28,3 +28,26 @@ Test(test_mylib, simple_test_div)
     cr_expect(mylib_div(150, 1) == 150, "the result is not what was expected.");
     cr_expect(mylib_div(100, 2) == 50, "the result is not what was expected.");
 }
+
+Test(test_mylib, simple_test_mod4)
+{
+    cr_expect(MOD4(4) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD4(12) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD4(16) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD4(32) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD4(3) == 3, "the resutl obtained is not what was expected.");
+    cr_expect(MOD4(8) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD4(6) == 2, "the result obtained is not what was expected.");
+
+}
+
+Test(test_mylib, simple_test_mod8)
+{
+    cr_expect(MOD8(7) == 7, "the result obtained is not what was expected.");
+    cr_expect(MOD8(8) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD8(16) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD8(24) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD8(32) == 0, "the result obtained is not what was expected.");
+    cr_expect(MOD8(34) == 2, "the result obtained is not what was expected.");
+    cr_expect(MOD8(38) == 6, "the result obtained is not what was expected.");
+}
